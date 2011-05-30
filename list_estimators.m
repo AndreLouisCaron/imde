7dc928e4323427d8917dd579b6cf333ad8f4b590
Andre Caron <andre.louis.caron@usherbrooke.ca>
2011-05-30 19:13:32 +0000

    Cleaned up estimators, added Newton's method.

--- list_estimators.m
+++ list_estimators.m
@@ -1,34 +1,29 @@
 function [ listing ] = list_estimators ()
     % Copyright(c) Andre Caron <andre.louis.caron@usherbrooke.ca>, 2009-2011
     
-    BP = @biquadratic_patch;
+    QP = @biquadratic_patch;
     CP = @bicubic_patch;
+    S = lattice([15 15]);
     listing = {
-      'bfs', @(Q,D,f,M,T) brute_force(...
-          cost_function(M,T), lattice([15 15]))
-          
-      'bfs-qp', @(Q,D,f,M,T) brute_force(...
-          cost_function(BP(M),T), lattice([15 15]))
-          
-      'bfs-cp', @(Q,D,f,M,T) brute_force(...
-          cost_function(CP(M),T), lattice([15 15]))
-
-      'si', @(Q,D,f,M,T) simplex(...
-          cost_function(CP(M),T), 2)
-
-      'si-qp', @(Q,D,f,M,T) simplex(...
-          cost_function(CP(M),T), 2)
-
-      'si-cp', @(Q,D,f,M,T) simplex(...
-          cost_function(CP(M),T), 2)
-
-      'nu', @(Q,D,f,M,T) newuoa(...
-          cost_function(CP(M),T), 2)
-
-      'nu-qp', @(Q,D,f,M,T) newuoa(...
-          cost_function(CP(M),T), 2)
-
-      'nu-cp', @(Q,D,f,M,T) newuoa(...
-          cost_function(CP(M),T), 2)
+      
+        % brute force search.
+      'bfs',    @(Q,D,f,M,T) brute_force(cost_function(    M,T), S)
+      'bfs-qp', @(Q,D,f,M,T) brute_force(cost_function(QP(M),T), S)
+      'bfs-cp', @(Q,D,f,M,T) brute_force(cost_function(CP(M),T), S)
+      
+        % simplex.
+      'si',    @(Q,D,f,M,T) simplex(cost_function(    M,T), 2)
+      'si-qp', @(Q,D,f,M,T) simplex(cost_function(QP(M),T), 2)
+      'si-cp', @(Q,D,f,M,T) simplex(cost_function(CP(M),T), 2)
+      
+        % NewUOA.
+      'nu',    @(Q,D,f,M,T) newuoa(cost_function(    M,T), 2)
+      'nu-qp', @(Q,D,f,M,T) newuoa(cost_function(QP(M),T), 2)
+      'nu-cp', @(Q,D,f,M,T) newuoa(cost_function(CP(M),T), 2)
+      
+        % Newton's method.
+      'nt-qp', @(Q,D,f,M,T) newtons_method(M,T,QP)
+      'nt-cp', @(Q,D,f,M,T) newtons_method(M,T,CP)
+      
       };
 end
