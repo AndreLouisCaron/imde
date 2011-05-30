8ef666b999aeaeb3576790de46d61dba15c2d5cc
Andre Caron <andre.louis.caron@usherbrooke.ca>
2011-05-30 19:30:36 +0000

    Force use of line vectors.

--- code/focus.m
+++ code/focus.m
@@ -1,5 +1,5 @@
 function [ m ] = focus ( M, B )
     % Copyright(c) Andre Caron <andre.louis.caron@usherbrooke.ca>, 2009-2011
     
-    m = @(x) M(B(:,1) + x'.*(B(:,2)-B(:,1)));
+    m = @(x) M(B(:,1)' + x.*(B(:,2)-B(:,1))');
 end
